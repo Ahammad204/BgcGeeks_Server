833a2d78f2442d5edab517d0ec2ba492fb87d6b1
Ahammad204 <Ahammadcode@gmail.com>
2024-03-09 21:59:00 +0600

    Created User Model

---
+++ models/user.model.ts
@@ -0,0 +1,113 @@
+import mongoose,{Document,Model,Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document{
+
+    name:string;
+    email: string;
+    password: string;
+    avatar:{
+        public_id: string;
+        url: string;
+
+    },
+    role: string;
+    isVerified: boolean;
+    courses: Array<{courseId: string}>;
+    comparePassword: (password: string) => Promise<boolean>;
+
+};
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, "Please Enter Your Name"],
+
+    },
+    email:{
+
+        type: String,
+        required: [true, "Please Enter Your Email"],
+        validate:{
+
+            validator: function(value:string){
+
+                return emailRegexPattern.test(value);
+
+            },
+
+            message: "Please Enter a valid Email"
+
+        },
+        unique: true,
+
+    },
+
+    password:{
+
+        type:String,
+        required: [true, "Please Enter Your Password"],
+        minlength: [6, "Password must be at least 6 characters"],
+        select: false,
+
+    },
+
+    avatar:{
+
+        public_id: String,
+        url: String,
+    
+    },
+    role: {
+
+        type: String,
+        default: "User",
+
+    },
+
+    isVerified:{
+
+        type: Boolean,
+        default: false,
+
+    },
+    courses:[
+        {
+
+            courseId: String,
+
+        }
+    ],
+
+
+},{timestamps:true});
+
+//Hash Password before Saving
+userSchema.pre<IUser>('save', async function(next){
+
+    if(!this.isModified('password')){
+
+        next();
+
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+
+});
+
+
+//Compare Password 
+userSchema.methods.comparePassword = async function (enteredPassword:string): Promise<boolean>{
+
+    return await bcrypt.compare(enteredPassword, this.password);
+
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema)
+
+export default userModel;
+
