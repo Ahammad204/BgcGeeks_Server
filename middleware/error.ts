484951553fb3f91e06c7df9fbdc608431a9e298c
Ahammad204 <Ahammadcode@gmail.com>
2024-03-09 21:05:29 +0600

    Handle Error code added

---
+++ middleware/error.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+
+module.exports = (err:any, req:Request, res:Response,next:NextFunction) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'Internal Server Error';
+
+    //Wrong MongoDB Id error
+
+    if(err.name === 'CastError') {
+
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+
+    }
+
+    //Duplicate key error
+    if(err.code === 11000){
+
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler (message,400);
+
+    }
+
+    //Wrong JWT Error
+
+    if(err.name === 'JsonWebTokenError'){
+
+        const message = `Json web token is invalid. Try Again`;
+        err = new ErrorHandler(message, 400);
+
+    }
+    
+    //JWT Expired Error
+
+    if(err.name === 'TokenExpiredError' ){
+
+        const message = `Json Web token is expired, Try Again`;
+        err = new ErrorHandler(message, 400);
+
+    }
+
+    err.status(err.statusCode).json({
+
+        success:false,
+        message:err.message
+
+    })
+
+}
