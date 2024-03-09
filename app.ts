ab057d8b7e0aaaabad52035cc974aef33205a614
Ahammad204 <Ahammadcode@gmail.com>
2024-03-09 21:17:14 +0600

    Handle CatchAsyncErro Functionality

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 //Body Parser
 app.use(express.json({ limit: "50mb" }));
@@ -25,10 +26,11 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-
 //Unknown Route
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
   const err = new Error(`Route ${req.originalUrl} not found`) as any;
   err.statusCode = 404;
   next(err);
 });
+
+app.use (ErrorMiddleware);
