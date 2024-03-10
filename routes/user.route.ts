7d02e75a25f64be70342c2be54afd83adca8e23b
Ahammad204 <Ahammadcode@gmail.com>
2024-03-11 01:24:29 +0600

    when logout delete data from redis

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,13 +1,19 @@
-import express  from "express";
-import { activeUser, loginUser, logoutUser, registrationUser } from "../controllers/user.controller";
+import express from "express";
+import {
+  activeUser,
+  loginUser,
+  logoutUser,
+  registrationUser,
+} from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 const UserRouter = express.Router();
 
-UserRouter.post('/registration', registrationUser);
+UserRouter.post("/registration", registrationUser);
 
-UserRouter.post('/activate-user', activeUser);
+UserRouter.post("/activate-user", activeUser);
 
-UserRouter.post('/login', loginUser);
+UserRouter.post("/login", loginUser);
 
-UserRouter.get('/logout', logoutUser);
+UserRouter.get("/logout", isAuthenticated, logoutUser);
 
 export default UserRouter;
