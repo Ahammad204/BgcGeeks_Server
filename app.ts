3990abeb57119036c9a565931e591b36fba0368b
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-07-25 16:57:39 +0600

    Update for server

--- app.ts
+++ app.ts
@@ -22,8 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: process.env.ORIGIN,
-    origin: ['http://localhost:3000'],
+    origin: process.env.CLIENT_URL || ['http://localhost:3000'],
     credentials: true,
   })
 );
